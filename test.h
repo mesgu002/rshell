b0608eefcb8b39968f04158b54f2e6b4f21b4464
Montana Esguerra <mesgu002@hammer.cs.ucr.edu>
2016-05-12 10:51:00 -0700

    added test.h

--- test.h
+++ test.h
@@ -1,16 +1,13 @@
 #ifndef TEST_H
 #define TEST_H
 
+#include <iostream>
+
+using namespace std
+
+void hello();
 //This is a test
 
-#endif#ifndef TEST_H
-#define TEST_H
 
-//This is a test
-
-#endif#ifndef TEST_H
-#define TEST_H
-
-//This is a test
 
 #endif
