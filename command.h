94afbada9baae769e0b7fc2d0d5f6279278615db
Montana Esguerra <mesgu002@ucr.edu>
2016-05-12 21:39:00 -0700

    command.h 1.1

--- command.h
+++ command.h
@@ -5,4 +5,26 @@
 
 using namespace std;
 
+class Base //Base Class
+{	
+	public:
+		virtual void execute() = 0; //pure virtual function
+};
+
+class Command : public Base  // The Command Class inherits from the Base Class
+{
+	public:
+		void execute() {};
+};
+
+class Connector : public Command // The Connector Class inherits from both the Command class and the Base Class.
+{
+	private:
+		Command left;
+		Command right;
+	
+	public:
+		virtual void execute() = 0;
+};
+
 #endif
