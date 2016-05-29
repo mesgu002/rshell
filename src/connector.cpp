41a9f5f4f741873f6c743460f4250d3f5e9bf072
Montana Esguerra <mesgu002@ucr.edu>
2016-05-29 16:03:46 -0700

    defined connector class

---
+++ src/connector.cpp
@@ -0,0 +1,17 @@
+#include "../header/connector.h"
+
+using namespace std;
+
+connector :: connector (base* lhs, base* rhs)
+{
+	left = lhs;
+	right = rhs;
+}
+
+connector :: connector ()
+{
+	left = NULL;
+	right = NULL:
+}
+
+#endif
